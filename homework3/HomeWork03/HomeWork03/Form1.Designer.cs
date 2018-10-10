bd530e590e91d5430164a38daa98d77ae4dc8683
Joey Faas <joeyfaas@gmail.com>
2018-10-10 18:28:04 -0400

    added homework03

---
+++ homework3/HomeWork03/HomeWork03/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(168, 28);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 29);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Select File";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(42, 28);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 77);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Process";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(168, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 29);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Save To";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(42, 131);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(234, 33);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 192);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ homework3/HomeWork03/HomeWork03/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Tracing;
+using log4net;
+using System.Windows.Forms;
+using System.Diagnostics;
+using FileProcessing;
+
+//example at https://stackoverflow.com/questions/9805281/writing-logs-to-file
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+                (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public string oFolder;
+        public string iFolder;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Select the input file
+        private void button4_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            iFolder = folderBrowserDialog1.SelectedPath;
+
+            string[] files = Directory.GetFiles(iFolder); // Add files within file path to array
+
+            
+            int i; // Index of last file path "\" character
+            string f; // File name
+
+            foreach (string fname in files)
+            {
+                i = fname.LastIndexOf('\\') + 1;
+                f = fname.Substring(i);
+                if (f.EndsWith(".txt")) {
+                    comboBox1.Items.Add(f);
+                    if (comboBox1  == null)
+                    {
+                        throw new System.ArgumentNullException("Please Select A File");
+                    }
+            
+                }
+            }
+        }
+
+        // Select the output file
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            oFolder = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void Form1_Load(object sender, EventArgs e){}
+
+        // Word Count
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string if1 = iFolder + "\\" + comboBox1.SelectedItem;
+            string of1 = oFolder + "\\FileThis.txt";
+
+            ProcessTxt a = new ProcessTxt();
+            a.doString(if1, of1);
+
+            Cursor.Current = Cursors.WaitCursor;
+            System.Threading.Thread.Sleep(2000);
+            Cursor.Current = Cursors.Default;
+
+            Process.Start(of1);
+            log.Info("done");
+            MessageBox.Show("Read from file logged");
+        }
+
+    }
+}
