4d267e21f0ee4d14e59623c060449ab42eefbb3c
Joseph Faas <jjf25@calvin.edu>
2018-11-07 10:44:25 -0500

    finished hwk8

--- Homework08/Homework08/Homework08/Program.cs
+++ Homework08/Homework08/Homework08/Program.cs
@@ -12,48 +12,59 @@ namespace Homework08
     {
         static void Main(string[] args)
         {
-            XmlDocument readfile = new XmlDocument();
-            readfile.Load("readfile.xml");
-
+            //read the input file form user
             Console.WriteLine("Enter xml File: ");
             string path = Console.ReadLine().ToString();
-            //string path = @"H:\Data202\Data202\Homework08\Homework08\Homework08\bin\Debug\readfile.xml";
 
+            //loads user input file as a xml document
+            XmlDocument readfile = new XmlDocument();
+            readfile.Load(path);
+
+            //??
             XmlReaderSettings settings = new XmlReaderSettings();
             settings.IgnoreComments = true;
 
+            //creates path to for the xmlreader
             XmlReader xmlIn = XmlReader.Create(path, settings);
 
+            // Select nodes starting at index question 0 and get count of Y and N
             int countN = readfile.SelectNodes("survey /question[0]/response[. = \"N\"]").Count;
             int countY = readfile.SelectNodes("survey/question[0]/response[. = \"Y\"]").Count;
+
+            // initiate count to go to the next question response
             int count = 1;
+
+            // while xmlIn is reading the file
             while (xmlIn.Read())
             {
+                // if loop if the name is question and th xmlin is a nodetype of element
                 if (xmlIn.NodeType == XmlNodeType.Element &&
                     xmlIn.Name == "question")
                 {
+                    //select the nodes of the Y and N at question index count && count the file
                     countN = readfile.SelectNodes("survey/question["+count+"]/response[. = \"N\"]").Count;
                     countY = readfile.SelectNodes("survey/question["+count+"]/ response[. = \"Y\"]").Count;
-                    
+                    // write out the result
                     Console.WriteLine("Question "+ count+ ":" + 
                         "Yes=" + countY + "  No=" + countN);
+                    // incrememnt count of the index to move on to the next question responses
                     count++;
                 }
    
             }
 
-            //option 2
-            //int countN = readfile.SelectNodes("survey/question[1]/response[. = \"N\"]").Count;
-            //int countY = readfile.SelectNodes("survey/question[1]/response[. = \"Y\"]").Count;
+            /*
+            option 2
+            int countN = readfile.SelectNodes("survey/question[1]/response[. = \"N\"]").Count;
+            int countY = readfile.SelectNodes("survey/question[1]/response[. = \"Y\"]").Count;
 
-            // int countN2 = readfile.SelectNodes("survey/question[2]/response[. = \"N\"]").Count;
-            //int countY2 = readfile.SelectNodes("survey/question[2]/response[. = \"Y\"]").Count;
+            int countN2 = readfile.SelectNodes("survey/question[2]/response[. = \"N\"]").Count;
+            int countY2 = readfile.SelectNodes("survey/question[2]/response[. = \"Y\"]").Count;
 
-            //Console.WriteLine("Question 1: Yes=" + countY + "  No=" + countN);
+            Console.WriteLine("Question 1: Yes=" + countY + "  No=" + countN);
 
-            //Console.WriteLine("Question 2: Yes=" + countY2 + "  No=" + countN2);
-            
-
+            Console.WriteLine("Question 2: Yes=" + countY2 + "  No=" + countN2);
+            */
         }
     }
 }
